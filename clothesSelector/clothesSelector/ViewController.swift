6a4a2dcd370e833016666d8ead39fd94395186b6
吉野　瑠 <s15549@salesio-sp.ac.jp>
2018-05-23 22:02:53 +0900

    プロジェクト作成

---
+++ clothesSelector/clothesSelector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  clothesSelector
+//
+//  Created by 吉野　瑠 on 2018/05/23.
+//  Copyright © 2018年 yoshino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
